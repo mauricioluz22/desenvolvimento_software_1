4a8272d4f0682ae995316b54caa999ad8dc4ef9e
mauricioluz22 <161383052+mauricioluz22@users.noreply.github.com>
2024-02-28 21:31:20 +0000

    first commit

---
+++ P1_JAVAPROPERTIES/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nHi");
+        String version = System.getProperty("os.version");
+        System.out.println(version);
+    }
+}
